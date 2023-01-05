6b5a9d51a88818e6cf0c44d4163e985b90ef19ed
madorakkusu <yanistam@hotmail.fr>
2023-01-05 01:13:10 +0100

    Few CSS tweaks

--- src/components/Reaction/Reaction.tsx
+++ src/components/Reaction/Reaction.tsx
@@ -5,7 +5,7 @@ import { ReactionType } from './types';
 const Reaction: FC<ReactionType> = ({ positionPercentageX, positionPercentageY, comment }) => {
   return (
     <div className='reaction' style={{ top: positionPercentageY, left: positionPercentageX }}>
-      {comment}
+      <p className='comment'>{comment}</p>
     </div>
   );
 };
